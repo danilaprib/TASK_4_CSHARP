92c7f95e501de7e3918ceb61b03a5d80c66a3bad
qwertTYz <daniil.prib@gmail.com>
2026-04-12 22:26:47 +0300

    initial commit

---
+++ Data/TaskFourContext.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace TASK_4_CSHARP.Data
+{
+    public class TaskFourContext : DbContext
+    {
+
+    }
+}
---
+++ Data/User.cs
@@ -0,0 +1,14 @@
+﻿namespace TASK_4_CSHARP.Data
+{
+    public class User
+    {
+        public int user_id { get; set; }
+
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string status { get; set; }
+         
+        public bool is_blocked{ get; set; }
+    }
+}
