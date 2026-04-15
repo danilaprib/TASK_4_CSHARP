cf1af9a8da5b4b4490730dfa27812e036fd890db
qwertTYz <daniil.prib@gmail.com>
2026-04-15 09:46:28 +0300

    verify token migration

--- Migrations/20260415064529_VerifyToken.Designer.cs
+++ Migrations/20260415064529_VerifyToken.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TASK_4_CSHARP.Data;
@@ -11,9 +12,11 @@ using TASK_4_CSHARP.Data;
 namespace TASK_4_CSHARP.Migrations
 {
     [DbContext(typeof(TaskFourContext))]
-    partial class TaskFourContextModelSnapshot : ModelSnapshot
+    [Migration("20260415064529_VerifyToken")]
+    partial class VerifyToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260415064529_VerifyToken.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TASK_4_CSHARP.Migrations
+{
+    /// <inheritdoc />
+    public partial class VerifyToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "verification_token",
+                table: "users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "verification_token",
+                table: "users");
+        }
+    }
+}
